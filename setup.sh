8668c8358e57bb9756512ded1e3ec01ef37d9441
Michaël De Coninck <example@com>
2021-01-13 19:52:37 +0100

    changes to the setup.sh file

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
-mkdir -p ~/.streamlitecho "[server]
+mkdir -p ~/.streamlit/
+echo "[general]
+email = \"email@com\"
+" > ~/.streamlit/credentials.toml
+echo "[server]
 headless = true
 port = $PORT
 enableCORS = false
